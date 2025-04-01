a692c43fc4f562817cd9f9b316f9a6bfbc0857a5
MatheusGMattoso <mgarcia3818@gmail.com>
2025-04-01 19:31:23 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Home from "./pages/Home"
+import Sidebar from "./components/Sidebar"
+
+function App() {
+
+  return (
+    <>
+      <Sidebar></Sidebar>
+    </>
+  )
+
+}
+
+export default App
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,18 @@
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+        <div className="perfil">
+            <h1>foto de perfil</h1>
+            <h1>nome de perfil</h1>
+        </div>
+        <div className="lista">
+            <ul>
+                <li><a href="#">Inventário</a></li>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Sidebar
